c789c3bfb28387da20b24181e33ae17b8494fb31
Tejas Kumar Yadaw <tejaskyadaw@gmail.com>
2026-06-12 19:54:55 +0530

    Update FileCom.java

--- FileCom.java
+++ FileCom.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 public class FileCom{
     public static void main(String[] args){
         Scanner abc = new Scanner(System.in);
+        System.out.println("==== File Compression Simulator ====");
+        System.out.println("Enter String: ");
         String file = abc.next();
         int ar[] = new int[26];
+         System.out.println("Original String: "+ file);
         for(int i=0; i<file.length(); i++){
             char ch = file.charAt(i);
             if(ch>='a' && ch<='z'){
@@ -12,8 +15,10 @@ public class FileCom{
         }
         for(int i=0; i<26; i++){
             if(ar[i]>0){
-                System.out.println((char)(i+'a') + ar[i]);
+                System.out.println("Compressed String: "+ (char)(i+'a') + ar[i]);
             }
         }
+        System.out.println("Decompressed String: "+file);
+        System.out.println("Decompression Successful!");
     }
 }
